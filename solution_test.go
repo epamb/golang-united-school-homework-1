952d8cd57e27796fa08b1eef65faf54e59ef2914
Vova Abramyan <vladimir.abramyan@gmail.com>
2022-02-15 23:07:14 +0300

    Solution lecture 00

---
+++ solution_test.go
@@ -0,0 +1,22 @@
+package solution
+
+import "testing"
+
+func TestGetMessage(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "Positive case",
+			want: "Hello 🗺️ !",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetMessage(); got != tt.want {
+				t.Errorf("GetMessage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
